38cca7035043e909e423ca638ad82dcd99a2b68f
Ignacio26fr <130396697+Ignacio26fr@users.noreply.github.com>
2023-10-14 00:11:46 -0300

    validacion

--- js/pagar.js
+++ js/pagar.js
@@ -1,4 +1,4 @@
-const tarjeta = document.getElementById('cvc');
+const tarjeta = document.getElementById('tarjeta');
 const cvc = document.getElementById('cvc');
 const nombre = document.getElementById('nombre');
 
@@ -16,28 +16,50 @@ function validarFormulario(event) {
     msjError += "<p>El numero de la tarjeta no puede estar vacio</p>"
         
 }
-if(numerosDeTarjetaMax != tarjeta.value.lenght) {
+if(numerosDeTarjetaMax != tarjeta.value.length) {
     error = true;
     msjError += "<p>El numero de la tarjeta no puede ser mas de 16</p>"
 }
 if(cvc.value == ""){
-    if(cvc.value < 3  ){
-        if(cvc.value == 999){
-            error= true;
+    error = true;
+    msjError += "<p>El cvc no fue ingresado correctamente</p>"
+}
+
+    if(cvc.value.length != 3 ){
+        error = true;
+        msjError += "<p>El cvc tiene que ser 3 numeros </p>"
+        console.log(cvc.value.lenght)
+    }
+        if(cvc.value == "999" || cvc.value == "000"){
+            error = true;
             msjError += "<p>El cvc no fue ingresado correctamente</p>"
         }
-        }
-    }
+        
+    
     
     if(nombre.value == ""){
         error = true;
         msjError += "<p>-El nombre no puede estar vacio</p>"
     }
 
-    if(error){
+    if(error) {
         document.getElementById('errores').innerHTML = msjError;
-        return false;
-    }else{
+        return false
+    } else {
+        
+        var modal = document.getElementById("ventanaModal");
+        var span = document.getElementsByClassName("close")[0];
+        modal.style.display = "block";
+        span.onclick = function () {
+            modal.style.display = "none";
+        }
+        window.onclick = function () {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+        event.preventDefault();
+        return true
 
     }
 
