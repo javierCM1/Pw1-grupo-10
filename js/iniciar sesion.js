230d9b8637a53c2e1f322268adbfbec8b9d1dbdb
javierCM1 <cabreramoreljavier@gmail.com>
2023-10-28 15:04:46 -0300

    .

---
+++ js/iniciar sesion.js
@@ -0,0 +1,28 @@
+const formularioInicioSesion = document.getElementById('form-inicio_sesion');
+let logueado = false;
+
+formularioInicioSesion.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const usuarioInput = document.getElementById('usuario')
+    const passwordInput = document.getElementById('password')
+
+    const usuario = usuarioInput.value.trim();
+    const password =  passwordInput.value.trim()
+
+    const passwordModificada = modificarContraseña(password);
+
+    let arrayUsuariosRegistrados = JSON.parse(localStorage.getItem('users'));
+
+    for (const usuarioRegistrado of arrayUsuariosRegistrados) {
+        if (usuarioRegistrado.user === usuario && usuarioRegistrado.pass === passwordModificada) {
+            logueado= true;
+            alert("usuario encontrado")
+            break; 
+        }else{
+            alert("usuario no encontrado")
+        }
+    }
+
+});
+
--- js/registrar.js
+++ js/registrar.js
@@ -31,11 +31,15 @@ formulario.addEventListener('submit', (event) => {
         if (password !== passwordRepetida) {
             throw new Error("Las contraseñas no coinciden");
         }
+
         if (arrayUsuarios.some((user) => user.user === usuario)) {
             throw new Error("Usuario en uso");
         }
 
-        const UsuarioRegistrado = new usuarios(usuario, password, passwordRepetida, fecha, email);
+        const passwordModificada = modificarContraseña(password);
+        const passwordRepetidaModificada = modificarContraseña(passwordRepetida);
+        
+        const UsuarioRegistrado = new usuarios(usuario, passwordModificada, passwordRepetidaModificada, fecha, email);
 
         arrayUsuarios.push(UsuarioRegistrado);
 
@@ -43,20 +47,35 @@ formulario.addEventListener('submit', (event) => {
         localStorage.setItem('users', userJSON);
 
         document.getElementById('modal').style.display = 'flex';
-        
+
     } catch (error) {
         alert(error.message);
     }
 
 });
 
-    function cerrarModal() {
+function cerrarModal() {
     document.getElementById('modal').style.display = 'none';
-    } 
-    
-    function naverga() {
+}
+
+function modificarContraseña(password){
+
+        const longitud = password.length;
+        const mitad = Math.floor(longitud / 2);
+        const primeraMitad = password.slice(0, mitad);
+        const segundaMitad = password.slice(mitad);
+        const contraseñaModificada = segundaMitad + primeraMitad;
+        return contraseñaModificada;
+
+} 
+
+const BOTON = document.getElementById('botonInico');
+BOTON.addEventListener('click', (Event) => {
+    Event.preventDefault();
     window.location.href = 'vista-principal.html';
-    }
+});
+
+
 
 
 
