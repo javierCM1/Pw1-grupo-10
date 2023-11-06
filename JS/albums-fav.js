e2fc16d7622cb527c83c5ffd380e40e1d4fb8d4a
Florencia Carvajal <florcar1998@gmail.com>
2023-11-06 11:48:13 -0300

    links a los albums

--- JS/albums-fav.js
+++ JS/albums-fav.js
@@ -1,52 +1,4 @@
-/* 
-function cargarAlbumesFavoritos() {
-    const galeriaImagenes = document.getElementById("galeria-imagenes");
-    
-    const arrayUsuarios = JSON.parse(localStorage.getItem('users'));
-    const usuarioLogueado = arrayUsuarios.find((usuario) => usuario.logueado);
-    
-    if (usuarioLogueado) {
-        usuarioLogueado.listaAlbumsFav.forEach((album) => {
-            const article = document.createElement("article");
-            article.className = "item-imagen";
-    
-            const img = document.createElement("img");
-            img.src = `imagenes/albumcover${album}.jpg`;
-            img.alt = album;
-    
-            const checkbox = document.createElement("input");
-            checkbox.type = "checkbox";
-            checkbox.className = "estrella-checkbox";
-            checkbox.id = `estrella${album}`;
-            checkbox.checked = true; 
 
-    
-            const label = document.createElement("label");
-            label.className = "estrella-label";
-            label.htmlFor = `estrella${album}`;
-    
-            article.appendChild(img);
-            article.appendChild(checkbox);
-            article.appendChild(label);
-    
-            galeriaImagenes.appendChild(article);
-    
-            checkbox.addEventListener("change", (event) => {
-                if (!checkbox.checked) {
-                    const index = usuarioLogueado.listaAlbumsFav.indexOf(album);
-                    if (index !== -1) {
-                        usuarioLogueado.listaAlbumsFav.splice(index, 1);
-                    }
-                    localStorage.setItem('users', JSON.stringify(arrayUsuarios));
-                    galeriaImagenes.removeChild(article);
-                }
-            });
-        });
-    }
-}
-
-cargarAlbumesFavoritos();
- */
 
 function mostrarAlbumesFavoritos() {
     const galeriaImagenes = document.getElementById("galeria-imagenes");
@@ -55,21 +7,20 @@ function mostrarAlbumesFavoritos() {
     const usuarioLogueado = arrayUsuarios.find((usuario) => usuario.logueado);
 
     if (usuarioLogueado) {
-        galeriaImagenes.innerHTML = ''; // Limpiar galería antes de cargar
+        galeriaImagenes.innerHTML = '';
 
         usuarioLogueado.listaAlbumsFav.forEach((album) => {
             const article = document.createElement("article");
             article.className = "item-imagen";
 
-            const img = document.createElement("img");
-            img.src = `imagenes/albumcover${album}.jpg`;
-            img.alt = album;
+            const img = document.createElement("div");           
+            img.innerHTML = `<a href="musica-sonando.html"> <img src="imagenes/albumcover${album}.jpg" data-id="${album}" alt="${album}" onclick="redireccionarYMostrarDatos('${album}')">`;
 
             const checkbox = document.createElement("input");
             checkbox.type = "checkbox";
             checkbox.className = "estrella-checkbox";
             checkbox.id = `estrella${album}`;
-            checkbox.checked = true; // Asegurarte de que el checkbox esté marcado
+            checkbox.checked = true; 
 
             const label = document.createElement("label");
             label.className = "estrella-label";
@@ -98,6 +49,35 @@ function mostrarAlbumesFavoritos() {
 mostrarAlbumesFavoritos();
 
 const estrellas = document.querySelectorAll(".estrella-checkbox");
+
+function agregarAMisAlbums(event) {
+    const arrayUsuarios = JSON.parse(localStorage.getItem('users'));
+    const checkbox = event.target;
+    const idEstrella = checkbox.getAttribute("data-id");
+
+    let usuarioLogueado;
+    for (const userId in arrayUsuarios) {
+        if (arrayUsuarios[userId].logueado === true) {
+            usuarioLogueado = arrayUsuarios[userId];
+            break;
+        }
+    }
+
+    if (usuarioLogueado) {
+        if (checkbox.checked) {
+            if (!usuarioLogueado.listaAlbumsFav.includes(idEstrella)) {
+                usuarioLogueado.listaAlbumsFav.push(idEstrella);
+            }
+        } else {
+            const index = usuarioLogueado.listaAlbumsFav.indexOf(idEstrella);
+            if (index !== -1) {
+                usuarioLogueado.listaAlbumsFav.splice(index, 1);
+            }
+        }
+
+        localStorage.setItem('users', JSON.stringify(arrayUsuarios));
+    }
+}
 
 
 estrellas.forEach(estrella => {
